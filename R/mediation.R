952e92a09c7adc33245123057a6da516a6bdedaa
jbodelet <julien.bodelet@jacobscenter.uzh.ch>
2023-03-06 12:22:25 +0100

    update help

--- R/mediation.R
+++ R/mediation.R
@@ -35,8 +35,8 @@ mediation <- function(y, x, M, covariates = NULL, ... ){
 #' n <- 200
 #' z <- rnorm(n)
 #' M <- z + rnorm(n)
-#' y <- 2 * z + 2 * M + rnorm(n)
-#' fit <- mediation(y, z, M, warmup = 2000, iter = 4000, chains = 4)
+#' delta <- c(2,1,3)
+#' y <- sapply( delta, function(d) d * z + 2 * M + rnorm(n) )
 MVmediation <- function(y, x, M, covariates = NULL, ... ){
   C <- cbind( rep(1, nrow(y) ), covariates ) # add a column of ones for the intercept
   dat <- list( n = nrow(y), p = ncol(y), d = ncol(C), y = y, x = x, M = M, C = C )
