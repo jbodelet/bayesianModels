11d923d74f65d149a6bd778a17a9b2fb057a5a52
jbodelet <julien.bodelet@jacobscenter.uzh.ch>
2023-03-06 15:07:00 +0100

    update

--- R/mediation.R
+++ R/mediation.R
@@ -9,13 +9,12 @@
 #' @export
 #' @examples 
 #' n <- 200
-#' p <- 3
-#' z <- matrix( rnorm(n*p), ncol = p )
-#' a <- c(1,2,3)
-#' b <- c(0,2,1)
-#' M <- c(z %*% a + rnorm(n))
-#' delta <- c(2,1,3)
-#' y <- sapply( 1:p, function(j) delta[j] * z[, j] + b[j] * M + rnorm(n) )
+#' z <- rnorm(n)
+#' a <- 1
+#' b <- 2
+#' M <- z * a + rnorm(n)
+#' delta <- 2
+#' y <- delta * z + b * M + rnorm(n)
 #' fit <- MVmediation(y, z, M, warmup = 2000, iter = 4000, chains = 4)
 mediation <- function(y, x, M, covariates = NULL, ... ){
   C <- cbind( rep(1, length(y) ), covariates ) # add a column of ones for the intercept
@@ -37,10 +36,13 @@ mediation <- function(y, x, M, covariates = NULL, ... ){
 #' @export
 #' @examples 
 #' n <- 200
-#' z <- rnorm(n)
-#' M <- z + rnorm(n)
+#' p <- 3
+#' z <- matrix( rnorm(n*p), ncol = p )
+#' a <- c(1,2,3)
+#' b <- c(0,2,1)
+#' M <- c(z %*% a + rnorm(n))
 #' delta <- c(2,1,3)
-#' y <- sapply( delta, function(d) d * z + 2 * M + rnorm(n) )
+#' y <- sapply( 1:p, function(j) delta[j] * z[, j] + b[j] * M + rnorm(n) )
 #' fit <- MVmediation(y, z, M, warmup = 2000, iter = 4000, chains = 4)
 MVmediation <- function(y, x, M, covariates = NULL, ... ){
   C <- cbind( rep(1, nrow(y) ), covariates ) # add a column of ones for the intercept
